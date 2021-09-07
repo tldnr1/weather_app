46fc225dd92b0f215faf49513fca3da43d8bcddc
tldnr1 <tldnr1@knu.ac.kr>
2021-09-07 17:46:21 +0900

    second commit (getCurrentPositionAsync Error)

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import Loading from "./Loading";
+import { Alert } from 'react-native';
+import * as Location from "expo-location";
 
-export default function App() {
-  return <Loading />;
+export default class extends React.Component {
+  getLocation = async() => {
+    try {
+      const {status} = await Location.requestForegroundPermissionsAsync();
+      console.log(status);
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
+    }
+  }
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    return <Loading />;
+  }
 }
