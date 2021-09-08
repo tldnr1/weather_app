e68b69b02c2abd6d6350d3b3e0b6fd48c9efc623
tldnr1 <tldnr1@knu.ac.kr>
2021-09-09 01:27:14 +0900

    위치 api 에러 해결 (accuracy high로 바꿔서 해결)

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Loading from "./Loading";
 import { Alert } from 'react-native';
+import Loading from "./Loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
   getLocation = async() => {
     try {
-      const {status} = await Location.requestForegroundPermissionsAsync();
-      console.log(status);
-      const location = await Location.getCurrentPositionAsync();
-      console.log(location);
+      await Location.requestForegroundPermissionsAsync();
+      const { 
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync({ accuracy:Location.Accuracy.High });
+      console.log(latitude, longitude);
     } catch (error) {
       Alert.alert("Can't find you", "So sad");
     }
