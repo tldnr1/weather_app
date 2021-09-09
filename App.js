6176002f206dd9d989466e6cecd2329dbb7a681a
tldnr1 <tldnr1@knu.ac.kr>
2021-09-09 21:28:06 +0900

    making Weather.js

--- App.js
+++ App.js
@@ -12,10 +12,19 @@ export default class extends React.Component {
     isLoading: true
   }
   getWeather = async(latitude, longitude) => {
-    const { data } = await axios.get(
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
-    )
-    this.setState({ isLoading:false, temp: data.main.temp })
+    );
+    this.setState({
+      isLoading:false,
+      condition: weather[0].main,
+      temp
+    });
   }
   getLocation = async() => {
     try {
@@ -23,7 +32,7 @@ export default class extends React.Component {
       const { 
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync({ accuracy:Location.Accuracy.High });
-      this.getWeather(latitude, longitude)
+      this.getWeather(latitude, longitude);
     } catch (error) {
       Alert.alert("Can't find you", "So sad");
     }
@@ -31,8 +40,9 @@ export default class extends React.Component {
   componentDidMount() {
     this.getLocation();
   }
+  
   render() {
-    const { isLoading, temp } = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
   }
 }
--- Weather.js
+++ Weather.js
@@ -1,21 +1,48 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import propTypes from "prop-types";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Weather({ temp }) {
     return(
         <View style={styles.container}>
-            <Text>{temp}</Text>
+            <View style={styles.halfContainer}>
+                <MaterialCommunityIcons name="weather-lightning-rainy" size={86} />
+                <Text style={styles.temp}>{temp}</Text>
+            </View>
+            <View style={styles.halfContainer}>
+                <Text>
+
+                </Text>
+            </View>
         </View>
     );
 }
 
 Weather.propTypes = {
-    temp:propTypes.number.isRequired
+    temp:propTypes.number.isRequired,
+    condition: propTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds"
+    ]).isRequired
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    temp: {
+        fontSize: 32,
+
+    },
+    halfContainer: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
